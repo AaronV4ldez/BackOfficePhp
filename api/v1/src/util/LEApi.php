347d26425a40ecb036287b6cfe08918918b2cd61
Aarón Valdez Garcia <valdezaaron04@gmail.com>
2024-08-06 12:05:49 -0600

    chore: arreglar la inserción en la bdd

--- api/v1/src/util/LEApi.php
+++ api/v1/src/util/LEApi.php
@@ -6,11 +6,14 @@
 
 namespace Util;
 
+use PDO;
+use PDOException;
+
 const LE_API_URL_USUARIO = "/Usuarios/"; // + sentri/email
 const LE_API_URL_CARS_CROSS = "/Cruces/@idu/vehiculos/@idv/Cruces/@fi/@ff";
 const LE_API_URL_RECARGA = "/Transacciones/@idu/vehiculos/@idv";
-//const LE_API_URL_CARS_CROSS_NEW = "/ConsultaCrucesTag/@tag/@fi/@ff";
-const LE_API_URL_CARS_CROSS_NEW = "/ConsultaCrucesTag/@tag/20240601/20240701";
+const LE_API_URL_CARS_CROSS_NEW = "/ConsultaCrucesTag/@tag/@fi/@ff";
+//const LE_API_URL_CARS_CROSS_NEW = "/ConsultaCrucesTag/@tag/20240601/20240701";
 
 
 class LEApi
@@ -209,46 +212,122 @@ class LEApi
     }
     
     //function new cruces
-    
+
     public static function getCarLatestCrossingsnew($tag)
     {
         $tpApiKey = $_ENV["TP_API_KEY"];
         $baseURL = $_ENV["TP_BASE_URL"];
-        $fullUrl = $baseURL . LE_API_URL_CARS_CROSS_NEW;
+        $fullUrlTemplate = $baseURL . LE_API_URL_CARS_CROSS_NEW;
+    
+        $allResults = []; // Array para acumular todos los resultados
+    
+        // Definir los diferentes rangos de fechas para las cinco búsquedas
+        $dateRanges = [
+            ['fi' => date("Ymd", strtotime("-31 days")), 'ff' => date("Ymd")],
+            ['fi' => date("Ymd", strtotime("-62 days")), 'ff' => date("Ymd", strtotime("-31 days"))],
+            ['fi' => date("Ymd", strtotime("-93 days")), 'ff' => date("Ymd", strtotime("-62 days"))],
+            ['fi' => date("Ymd", strtotime("-124 days")), 'ff' => date("Ymd", strtotime("-93 days"))],
+            ['fi' => date("Ymd", strtotime("-155 days")), 'ff' => date("Ymd", strtotime("-124 days"))],
+            ['fi' => date("Ymd", strtotime("-186 days")), 'ff' => date("Ymd", strtotime("-155 days"))],
+            ['fi' => date("Ymd", strtotime("-217 days")), 'ff' => date("Ymd", strtotime("-186 days"))],
+            ['fi' => date("Ymd", strtotime("-248 days")), 'ff' => date("Ymd", strtotime("-217 days"))],
+            ['fi' => date("Ymd", strtotime("-279 days")), 'ff' => date("Ymd", strtotime("-248 days"))],
+            ['fi' => date("Ymd", strtotime("-310 days")), 'ff' => date("Ymd", strtotime("-279 days"))],
+            ['fi' => date("Ymd", strtotime("-341 days")), 'ff' => date("Ymd", strtotime("-310 days"))],
+            ['fi' => date("Ymd", strtotime("-372 days")), 'ff' => date("Ymd", strtotime("-341 days"))],
+            ['fi' => date("Ymd", strtotime("-403 days")), 'ff' => date("Ymd", strtotime("-372 days"))],
+            ['fi' => date("Ymd", strtotime("-434 days")), 'ff' => date("Ymd", strtotime("-403 days"))]
+        ];
+    
+        foreach ($dateRanges as $index => $range) {
+            $fullUrl = \str_replace("@tag", $tag, $fullUrlTemplate);
+            $fullUrl = \str_replace("@fi", $range['fi'], $fullUrl);
+            $fullUrl = \str_replace("@ff", $range['ff'], $fullUrl);
+    
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $fullUrl);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            $headers = array();
+            $headers[] = "Authorization:Bearer " . $tpApiKey;
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            $result = curl_exec($ch);
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+    
+            // Verificar si la solicitud fue exitosa
+            if ($httpCode == 200) {
+                $result = \json_decode($result, true);
+                if ($result !== null) {
+                    // Acumular los resultados obtenidos
+                    $allResults = array_merge($allResults, $result);
+    
+                    foreach ($result as $record) {
+                        self::insertdb($record);
+                    }
+                } else {
+                    error_log("Error decoding JSON response for range index $index: " . json_last_error_msg());
+                }
+            } else {
+                error_log("Error fetching data for range index $index: HTTP $httpCode");
+            }
+        }
+    
+        // Devolver todos los resultados en un solo JSON
+        header('Content-Type: application/json');
+        echo json_encode($allResults, JSON_PRETTY_PRINT);
+    }
 
-        $fullUrl = \str_replace("@tag", $tag, $fullUrl);
-        $fullUrl = \str_replace("@fi", date("Ymd", \strtotime("-31 days")), $fullUrl);
-        $fullUrl = \str_replace("@ff", date("Ymd"), $fullUrl);
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $fullUrl);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        $headers = array();
-        $headers[] = "Authorization:Bearer " . $tpApiKey;
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $result = curl_exec($ch);
-        curl_close($ch);
+    public static function insertdb($record)
+    {
+        try {
+            $dsn = 'mysql:host=127.0.0.1;dbname=LIEX_QA;charset=utf8';
+            $username = 'liex_user';
+            $password = 'line@infernal656';
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+            ];
 
-        // order result array by fechaHoraCruce field
-        $result = \json_decode($result, true);
-        \usort($result, function ($a, $b) {
-            return \strtotime($b["fechaHoraCruce"]) - \strtotime($a["fechaHoraCruce"]);
-        });
+            $pdo = new PDO($dsn, $username, $password, $options);
 
-        $result = \json_encode($result);
+            // Verificar si los datos ya existen
+            $stmt = $pdo->prepare("SELECT COUNT(*) FROM historical WHERE idOperador = :idOperador AND fechaHoraCruce = :fechaHoraCruce");
+            $stmt->bindParam(':idOperador', $record['idOperador'], PDO::PARAM_INT);
+            $stmt->bindParam(':fechaHoraCruce', $record['fechaHoraCruce'], PDO::PARAM_STR);
+            $stmt->execute();
+            $count = $stmt->fetchColumn();
 
-        if ($result === false) {
-            return \json_encode([]);
-        } else {
-            return $result;
+            if ($count == 0) {
+                // Insertar si no existe
+                $stmt = $pdo->prepare("INSERT INTO historical (idOperador, nombreOperador, siglasOperador, clavePlaza, nombrePlaza, descripcionPlaza, tipoTransito, numeroCarril, fechaHoraCruce, montoTarifa, numeroTag, claseVehiculo, tipoVehiculo) VALUES (:idOperador, :nombreOperador, :siglasOperador, :clavePlaza, :nombrePlaza, :descripcionPlaza, :tipoTransito, :numeroCarril, :fechaHoraCruce, :montoTarifa, :numeroTag, :claseVehiculo, :tipoVehiculo)");
+                $stmt->bindParam(':idOperador', $record['idOperador'], PDO::PARAM_INT);
+                $stmt->bindParam(':nombreOperador', $record['nombreOperador'], PDO::PARAM_STR);
+                $stmt->bindParam(':siglasOperador', $record['siglasOperador'], PDO::PARAM_STR);
+                $stmt->bindParam(':clavePlaza', $record['clavePlaza'], PDO::PARAM_STR);
+                $stmt->bindParam(':nombrePlaza', $record['nombrePlaza'], PDO::PARAM_STR);
+                $stmt->bindParam(':descripcionPlaza', $record['descripcionPlaza'], PDO::PARAM_STR);
+                $stmt->bindParam(':tipoTransito', $record['tipoTransito'], PDO::PARAM_STR);
+                $stmt->bindParam(':numeroCarril', $record['numeroCarril'], PDO::PARAM_INT);
+                $stmt->bindParam(':fechaHoraCruce', $record['fechaHoraCruce'], PDO::PARAM_STR);
+                $stmt->bindParam(':montoTarifa', $record['montoTarifa'], PDO::PARAM_INT);
+                $stmt->bindParam(':numeroTag', $record['numeroTag'], PDO::PARAM_STR);
+                $stmt->bindParam(':claseVehiculo', $record['claseVehiculo'], PDO::PARAM_STR);
+                $stmt->bindParam(':tipoVehiculo', $record['tipoVehiculo'], PDO::PARAM_STR);
+                $stmt->execute();
+
+                //echo "Data inserted successfully!";
+            } else {
+                //echo "Data already exists in the database.";
+            }
+        } catch (PDOException $e) {
+            //echo "Error: " . $e->getMessage();
         }
     }
 
-    public function insertdb(){
-        
-    }
     
 }
